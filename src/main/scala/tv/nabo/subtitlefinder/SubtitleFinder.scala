eb25561391586f6607ad9769b87d8bad69a69640
Matt Hicks <matt@outr.com>
2018-06-06 20:05:34 -0500

    Added support for recursive directory scanning

--- src/main/scala/tv/nabo/subtitlefinder/SubtitleFinder.scala
+++ src/main/scala/tv/nabo/subtitlefinder/SubtitleFinder.scala
@@ -1,51 +1,92 @@
 package tv.nabo.subtitlefinder
 
-import java.io.File
+import java.io.{File, FileNotFoundException}
 import java.net.{HttpURLConnection, URL}
 import java.nio.charset.Charset
-import java.util.zip.ZipFile
+import java.util.zip.{ZipEntry, ZipFile}
 
 import org.powerscala.io._
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object SubtitleFinder {
+  private val videoExtensions = Set("avi", "mkv")
+
   def main(args: Array[String]): Unit = {
     if (args.length == 1) {
-      val video = new File(args.head)
-      val hash = OpenSubtitlesHasher.computeHash(video)
-      val rpc = new OpenSubtitlesRPC()
-      val future = rpc.login().flatMap { token =>
-        rpc.search(token, hash, video.length())
-//        rpc.quickSuggest(token, "the magnificent seven 2016")
-      }
-      val matches = Await.result(future, 30.seconds)
-      if (matches.isEmpty) {
-        sys.error("No subtitle matches found!")
-      } else {
-        val subZip = new File("subtitles.zip")
-        IO.stream(new URL(matches.head), subZip)
+      val file = new File(args.head)
+      process(file)
+    } else {
+      println(s"Usage: subtitle_finder.jar PATH_TO_VIDEO")
+    }
+  }
 
-        val zip = new ZipFile(subZip)
-        try {
-          val srtEntry = zip.entries().asScala.find(_.getName.toLowerCase.endsWith(".srt")).getOrElse(throw new RuntimeException("No SRT file found in ZIP!"))
-          val videoName = video.getName.substring(0, video.getName.lastIndexOf('.'))
-          val srtFile = new File(video.getParentFile, s"$videoName.srt")
-          val input = zip.getInputStream(srtEntry)
-          IO.stream(input, srtFile)
-        } finally {
-          zip.close()
-          if (!subZip.delete()) {
-            subZip.deleteOnExit()
-          }
+  implicit class FileExtras(f: File) {
+    lazy val extension: String = f.getName.substring(f.getName.lastIndexOf('.') + 1)
+  }
+
+  implicit class ZipFileExtras(f: ZipEntry) {
+    lazy val extension: String = f.getName.substring(f.getName.lastIndexOf('.') + 1)
+  }
+
+  def process(video: File): Unit = {
+    if (video.isDirectory) {
+      video.listFiles().foreach { f =>
+        if (videoExtensions.contains(f.extension)) {
+          process(f)
+        } else if (f.isDirectory) {
+          process(f)
         }
       }
     } else {
-      println(s"Usage: subtitle_finder.jar PATH_TO_VIDEO")
+      val videoName = video.getName.substring(0, video.getName.lastIndexOf('.'))
+      val srtFile = new File(video.getParentFile, s"$videoName.srt")
+      if (srtFile.exists()) {
+        println(s"Skipping ${video.getName}, it already has subtitles")
+      } else {
+        println(s"Processing ${video.getName}...")
+        val hash = OpenSubtitlesHasher.computeHash(video)
+        val rpc = new OpenSubtitlesRPC()
+        val future = rpc.login().flatMap { token =>
+          rpc.search(token, hash, video.length())
+          //        rpc.quickSuggest(token, "the magnificent seven 2016")
+        }
+        val matches = Await.result(future, 30.seconds)
+        if (matches.isEmpty) {
+          sys.error("No subtitle matches found!")
+        } else {
+          val subZip = new File("subtitles.zip")
+
+          try {
+            IO.stream(new URL(matches.head), subZip)
+
+            val zip = new ZipFile(subZip)
+            val extensions = List("srt", "sub", "txt")
+            try {
+              val entries = zip.entries().asScala.toList
+              extensions.flatMap { ext =>
+                entries.find(_.extension == ext)
+              }.headOption match {
+                case Some(srtEntry) => {
+                  val input = zip.getInputStream(srtEntry)
+                  IO.stream(input, srtFile)
+                }
+                case None => println(s"No SRT file found in ZIP (${entries.mkString(", ")})!")
+              }
+            } finally {
+              zip.close()
+              if (!subZip.delete()) {
+                subZip.deleteOnExit()
+              }
+            }
+          } catch {
+            case exc: FileNotFoundException => println(s"Unable to find subtitles for ${video.getName} (${exc.getMessage})")
+          }
+        }
+      }
     }
   }
 }
